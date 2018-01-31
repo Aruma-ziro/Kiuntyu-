9ef5e79d3d07737f8aa3cd2b1e73c5a4ce2c53ea
BuildTools <unconfigured@null.spigotmc.org>
2018-01-31 22:50:57 +0900

    mypage

--- js/mypage.js
+++ js/mypage.js
@@ -8,19 +8,22 @@ var config = {
 };
 firebase.initializeApp(config);
 var provider = new firebase.auth.FacebookAuthProvider();
-var uid = "test1/"
+var uid = user.uid
+var ref = firebase.database().ref("/users/test1");
+
+
 var username;
 var profile;
 var leaderEvent;
 var memberEvent;
 firebase.auth().onAuthStateChanged(function(user) {
-    firebase.database().ref("users/test1/name").on("value",function(snapshot){
-        username = snapshot.val().name;
-        document.getElementById("name").innerHTML = "名前：" + username;
-    });
-    firebase.database().ref("users/test1/profile").on("value",function(snapshot){
-        profile = snapshot.val().profile;
-        document.getElementById("profile").innerHTML = "プロフィール：" + profile;
+    ref.on("value",function(snapshot){
+
+        var username = snapshot.child("name").val();
+        document.getElementById("u_name").innerHTML = "名前：" + username;
+
+        var profile = snapshot.child("profile").val();
+        document.getElementById("u_profile").innerHTML = "プロフィール：" + profile;
     });
     firebase.database().ref("users/" + uid +"/events"+"/leader").on("value",function(snapshot){
         leaderEvent = snapshot.val();
