334a90f934ff6e0685d54c0aa2f7c9893b9c5664
BuildTools <unconfigured@null.spigotmc.org>
2018-01-31 21:40:48 +0900

    mypage

---
+++ mypage.js
@@ -0,0 +1,30 @@
+var config = {
+ apiKey: "AIzaSyB72JSMwvhh25ac-HDcpe9kK8xAZ8vTxVQ", 
+ authDomain: "kiunchu-bf551.firebaseapp.com", 
+ databaseURL: "https://kiunchu-bf551.firebaseio.com", 
+ projectId: "kiunchu-bf551", 
+ storageBucket: "kiunchu-bf551.appspot.com", 
+ messagingSenderId: "272941572629" 
+};
+firebase.initializeApp(config);
+var provider = new firebase.auth.FacebookAuthProvider();
+var uid = "test1/"
+var username;
+var profile;
+var leaderEvent;
+var memberEvent;
+firebase.auth().onAuthStateChanged(function(user) {
+    firebase.database().ref("users/" + uid +"/username").on("value",function(snapshot){
+        username = snapshot.val();
+        document.getElementById("name").innerHTML = "名前：" + username;
+    });
+    firebase.database().ref("users/" + uid +"/profile").on("value",function(snapshot){
+        profile = snapshot.val();
+        document.getElementById("profile").innerHTML = "プロフィール：" + profile;
+    });
+    firebase.database().ref("users/" + uid +"/events"+"/leader").on("value",function(snapshot){
+        leaderEvent = snapshot.val();
+    });
+    firebase.database().ref("users/" + uid +"/events"+"/member").on("value",function(snapshot){
+        memberEvent = snapshot.val();
+    });
