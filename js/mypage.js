381dac95ff85270709b8844615a7c19d7d9787dd
BuildTools <unconfigured@null.spigotmc.org>
2018-02-01 00:22:51 +0900

    mypage

--- js/mypage.js
+++ js/mypage.js
@@ -1,33 +1,46 @@
 var config = {
- apiKey: "AIzaSyB72JSMwvhh25ac-HDcpe9kK8xAZ8vTxVQ", 
- authDomain: "kiunchu-bf551.firebaseapp.com", 
- databaseURL: "https://kiunchu-bf551.firebaseio.com", 
- projectId: "kiunchu-bf551", 
- storageBucket: "kiunchu-bf551.appspot.com", 
- messagingSenderId: "272941572629" 
+   apiKey: "AIzaSyB72JSMwvhh25ac-HDcpe9kK8xAZ8vTxVQ", 
+   authDomain: "kiunchu-bf551.firebaseapp.com", 
+   databaseURL: "https://kiunchu-bf551.firebaseio.com", 
+   projectId: "kiunchu-bf551", 
+   storageBucket: "kiunchu-bf551.appspot.com", 
+   messagingSenderId: "272941572629" 
 };
 firebase.initializeApp(config);
+
 var provider = new firebase.auth.FacebookAuthProvider();
-var uid = user.uid
-var ref = firebase.database().ref("/users/test1");
+console.log(provider);
 
 
-var username;
-var profile;
+var ref;
+
 var leaderEvent;
 var memberEvent;
+
 firebase.auth().onAuthStateChanged(function(user) {
-    ref.on("value",function(snapshot){
+    var uid = user.uid;
+    if (user){
 
-        var username = snapshot.child("name").val();
-        document.getElementById("u_name").innerHTML = "名前：" + username;
+        firebase.database().ref("users/" + uid +"/events"+"/leader").on("value",function(snapshot){
+            leaderEvent = snapshot.val();
+        });
+        firebase.database().ref("users/" + uid +"/events"+"/member").on("value",function(snapshot){
+            memberEvent = snapshot.val();
+        });
+    }
 
-        var profile = snapshot.child("profile").val();
-        document.getElementById("u_profile").innerHTML = "プロフィール：" + profile;
-    });
-    firebase.database().ref("users/" + uid +"/events"+"/leader").on("value",function(snapshot){
-        leaderEvent = snapshot.val();
-    });
-    firebase.database().ref("users/" + uid +"/events"+"/member").on("value",function(snapshot){
-        memberEvent = snapshot.val();
-    });
+    else{
+
+        ref = firebase.database().ref("/users/test1");
+
+        ref.on("value",function(snapshot){
+
+            var username = snapshot.child("name").val();
+            document.getElementById("u_name").innerHTML = "名前：" + username;
+
+            var profile = snapshot.child("profile").val();
+            document.getElementById("u_profile").innerHTML = "プロフィール：" + profile;
+        });
+
+    }
+    
